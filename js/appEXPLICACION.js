31f9960dab4ecda9e931bf42df0cebc59561d982
Fernando Barahona <fernandobarahonad@gmail.com>
2019-02-27 19:08:26 -0500

    crear acrchivo de explicacion
    Cree un archivo de explicacion para poner comentarios que son medio molestos a la vista que expliquen que hago.
    
    Tambien corregi un problema en el punto para cuando es cero.

--- js/appEXPLICACION.js
+++ js/appEXPLICACION.js
@@ -1,6 +1,6 @@
 var Calculadora = (function(){
     "use strict";
-    //declaracion e inicializacion de variables globales
+    //declaracion e inicializacion de VARIABLES GLOBALES
     var numeroTexto = '0';
     var numeroActual = 0;
     var numeroAnterior = 0;
@@ -13,6 +13,7 @@ var Calculadora = (function(){
     //presento los titulos en consola para la revision 
     console.log('teclPres',"----",'numAnter',"----",'opAnter',"----",'numAct',"---------",'numeroTexto');   
 
+    //declaracion de TODAS LAS FUNCIONES
 
     //funcion llamada al hacer click
     var funcionTeclaPresionada = function(ev){
@@ -47,6 +48,10 @@ var Calculadora = (function(){
                 procesarTeclaIgual();
                 break;
         }
+        {/*
+            Ver la exlicacion de auxiliarSigno en la funcion cambiarSigno().
+            Aca lo reseteo cuando aplasto otra tecla.
+        */}
         if (teclaPresionada != 'sign'){
             auxiliarSigno = false;
         }
@@ -54,9 +59,17 @@ var Calculadora = (function(){
     }
     //funcion para procesar cuando aplasto =
     var procesarTeclaIgual = function(){
+        {/*
+            Proceso con la operacion anterior el numAnterior y el numActual.
+                Cuando aplaste igual antes, proceso por con mismo numeroActual de antes, 
+                por esto necesito auxiliarIgual, que no se cambia cuando se aplasta la segunda vez igual.
+            De ahi siempre se comprueba el largo, se configura numeroTexto se pasa el numActual al numAnterior
+                y se presenta en el display. Pero si el numero fue muy grande al comprobar, encero la calcu
+                presento una alerta y reseteo la bantera numTooBig. (ver comprobar largo)
+        */}
         if(teclaAnterior == 'igual'){
             numeroActual = operar(operacionAnterior,numeroAnterior,auxiliarIgual);
-        }
+        } 
         else{
             auxiliarIgual = numeroActual;
             numeroActual = operar(operacionAnterior,numeroAnterior,numeroActual);
@@ -73,6 +86,15 @@ var Calculadora = (function(){
     }
     //funcion para procesar cuando aplasto +-*/ 
     var procesarTeclaOperacion = function(teclaPresionada){
+        {/*
+            Siempre pongo el numTexto = '' para presentarlo y resetear la entrada del numero siguiente;
+                guardo la operacion que voy a hacer luego que es igual a teclaPresionada
+                Y compruebo si la alerta NumTooBig esta activa, en cuyo caso reseteo (ver comprobarLargo()).
+            Si aplaste igual y luego aplasto una operacion, no pasa nada mas que eso (tengo que ponerle porque el otro if 
+                es un if de != a otros y 'igual' tambien es diferente)
+            Si aplaste signo es como estar recien tipeado un numero entonces no proceso pero si grabo numAct en NumAnter
+            Y si recien esta tipeado un numero opero y compruebo y luego grabo numAct en NumAnter
+        */}
         if(teclaAnterior == 'igual'){
         }
         else if (teclaAnterior == 'sign'){
@@ -94,6 +116,12 @@ var Calculadora = (function(){
     }
     //funcion para comprobar el largo
     var comprobarLargo = function(numero){
+        {/*
+            Compruebo si la longitud es mayor. Si si: 
+            Si el valor absoluto es mayor que 99999999 osea ya no se puede presentar en la pantalla activo la bandera numTooBig
+                que uso en procesarIgua y procesarTeclaOperacion.
+            Si solo es por los decimales entonces solo dejo 8 caracteres.
+        */}
         if(String(numero).length >8){
             if(numero>99999999 || numero<-99999999){
                 numTooBig = true;
@@ -105,6 +133,14 @@ var Calculadora = (function(){
     }
     //funcion para cambiar el signo. 
     var cambiarSigno = function(){
+        {/*
+            No se pone signo cuando aplasto +-, esto lo hago con if (teclaAnterior = '+') etc, 
+                pero si vuelvo a aplastar signo tampoco tengo que poner. Entonces uso auxiliarSigno, 
+                el cual se resetea arriba, al principio del codigo.
+            Para poner el signo verifico si tiene signo para quitar el signo de numeroTexto. Tambien verifico si es cero para que el 
+                cero no tenga signo. 
+            Y cambiar el signo de NumAct es facil, solo igual a - y presento de una vez. 
+        */}
         if (teclaAnterior == 'mas' ||teclaAnterior == 'menos' ||teclaAnterior == 'por' ||teclaAnterior == 'dividido' ||auxiliarSigno == true){
             auxiliarSigno = true;
             numeroTexto = '';
@@ -119,6 +155,11 @@ var Calculadora = (function(){
     }
     //funcion para comprobar si ya hay un punto y si no lo hay anadir un punto en el numero
     var anadirPunto = function(){
+        {/*
+            Compruebo si es 0 el numero porque no puedo pasar . a generar numero porque . no es un numero.
+            Compruebo si el numero ya tiene punto y no hago nada. 
+            Sino genero el numero actual y de paso ya lo presento con el punto.
+        */}
         if(numeroTexto == '0'|| numeroTexto == ''){
             numeroActual = 0.0;
             numeroTexto = '0.'
@@ -132,6 +173,11 @@ var Calculadora = (function(){
     }
     //funcion para recojer cada numero presionado y generar numero actual
     var generarNumeroActual = function(teclaPresionada){
+        {/*
+            Solo genero si tiene menos de 9 caracteres. 
+            Si es 0 o -0, el numero es igual a la tecla presionada.
+            Caso contrario anado el caracter al numero y lo vuelvo a transformar a tipo Number
+        */}
         if (numeroTexto.length < 8){
             if(numeroTexto == "0"||numeroTexto == "-0"){numeroTexto = teclaPresionada}
             else{numeroTexto += teclaPresionada;}
@@ -140,6 +186,10 @@ var Calculadora = (function(){
     }
     //funcion para reestablecer los valores iniciales de las variables
     var encerarCalculadora = function(){
+        {/*
+            Pongo todos los valores de las variables globales a sus valores iniciales
+                y reseteo la consola y le pongo los titulos de la consola. 
+        */}
         numeroActual = 0;
         numeroTexto = '0';
         numeroAnterior = 0;
@@ -176,6 +226,11 @@ var Calculadora = (function(){
     }
 
     //funciones para animar las Teclas
+        {/*
+            Le achico y anado un listener de quitar el mouse.
+            Luego en agrandar (que pasa cuando quito el mouse)
+                quito el listener para ahorrar recursos del sistema 
+        */}
     var achicarTecla = function(ev){
         ev.target.style.transform = "scale(.9,.9)";
         document.addEventListener("mouseup", agrandarTecla);
@@ -184,6 +239,12 @@ var Calculadora = (function(){
         ev.target.style.transform = "scale(1,1)";
         document.removeEventListener("mouseup", agrandarTecla);
     }
+
     //listener que escucha que se presiono una tecla
+    {/* 
+        Como es una funcion autollamada se ejecuta de una. 
+        Y esta la unica linea de codigo que no es una declaracion de variable o funcion, 
+            esta activa a las demas funciones y utiliza las variables
+    */}
     document.addEventListener("mousedown",funcionTeclaPresionada)
 })();
